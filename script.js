8d7538264d4b5b2d637a330ff4435ba304832e9b
Reyna Ayala <55515949+reyna-ayala@users.noreply.github.com>
2024-07-14 00:26:56 -0400

    Changed tile layer to Mapbox

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?pk.eyJ1IjoicmV5bmFqYSIsImEiOiJjbHlsMWozNmUwamIzMmtvZ2MzM2pvbG1kIn0.az6-coAWRqug_CR7RwgPWg', {
+  attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+  id: 'mapbox://styles/reynaja/clyl1kyv601vv01qo0ijj5607', // Replace with your Mapbox style ID
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoicmV5bmFqYSIsImEiOiJjbHlsMWozNmUwamIzMmtvZ2MzM2pvbG1kIn0.az6-coAWRqug_CR7RwgPWg'
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map)
